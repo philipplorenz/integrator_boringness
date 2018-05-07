d2cba22681f584d6d53bf4aade4d3ae4b5d2e937
philipplorenz <philipp.lorenz@tu-berlin.de>
2018-05-07 10:14:52 +0200

    simplified version

--- integrator_boringness_model.py
+++ integrator_boringness_model.py
@@ -68,39 +68,7 @@ for i in range(len(y0)/2):
 	traj1 = [s[i*2] for s in sol]
 	#plot the trajectories for checking
 	plt.plot(traj1, lw=3)
-	traj = []
-	for t in range(len(traj1)):
-		if t%(1.0/t_step) == 0:
-			#traj.append(sum(traj1[t:t+int(1.0/t_step)]))
-			traj.append(traj1[t])
-	trajectories.append(traj)
 
 plt.xlabel('$t$', fontsize=20)
 plt.ylabel('$L_i(t)$', fontsize=20)
-
-#collect relative changes for saving
-final_list = [[] for t in range(len(trajectories[0]))]
-final_list_reverse = [[] for t in range(len(trajectories[0]))]
-for traj in trajectories:
-
-	if max(traj) > 0.0:	
-		for j in range(len(traj)-1):
-			#do not consider the first thousand steps
-			if j > 3000:
-				#avoid overflow in division
-				if j != 0 and traj[j-1] > 1e-5 and traj[j+1] > 1e-5:
-				
-					change = (traj[j] - traj[j-1])/float(traj[j-1])
-					final_list[j/1].append([i, traj[j], change])
-					change_reverse = (traj[j] - traj[j+1])/float(traj[j+1])
-					final_list_reverse[j/1].append([i, traj[j], change_reverse])
-
-#save data using pickle in various files
-with open("./trajectory_simulated_{0}.txt".format(r), "wb") as fp:   #Pickling
-	pickle.dump(trajectories[:], fp)
-with open("./plotdata_simulated_{0}.txt".format(r), "wb") as fp:   #Pickling
-	pickle.dump(final_list[:], fp)
-with open("./plotdata_simulated_reverse_{0}.txt".format(r), "wb") as fp:   #Pickling
-	pickle.dump(final_list_reverse[:], fp)
-
 plt.show()
