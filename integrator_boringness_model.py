95aabdeee662e5fd2dc949a53bed7ccec881fef1
philipplorenz <philipp.lorenz@tu-berlin.de>
2018-11-27 15:47:27 +0100

    added output

--- integrator_boringness_model.py
+++ integrator_boringness_model.py
@@ -1,72 +1,107 @@
 import numpy as np
-import scipy.integrate
-import sys
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import sys
+import pandas as pd
+import json
 import pickle
+import scipy.integrate
 
-#the model
-def system(t, y, a, c, r):
+def f(t, y, params):
+    L = y[:len(y)/2]
+    Y = y[len(y)/2:]
+    a, c, r = params  # unpack parameters
+    
+    l_sum = sum(L)
+    dL = []
+    dY = []
+    for i in range(len(L)):
+	dL.append(r*L[i]*(1.0 - (r*Y[i] + c*(l_sum-L[i]))))
+        dY.append(L[i] - a*Y[i])
 	
-	L = [y[i*2] for i in range(len(y)/2)]
-	Y = [y[i*2+1] for i in range(len(y)/2)]
-		
-	dydt = []
-	l_sum = sum(L)
-	for i in range(len(y)/2):
-		
-		dydt.append(1.0*r*L[i]*(1.0 - (r*Y[i] + c*(l_sum-L[i]))))
-		dydt.append(L[i] - a*Y[i])
+    derivs = dL + dY
+    return derivs
 
-	return dydt
-
-#parameters from command line
+# Parameters
 a = float(sys.argv[1]) #0.005
 c = float(sys.argv[2]) #2.4
 r = float(sys.argv[3]) #12.0
 size = int(sys.argv[4]) #300
-#length and granularity of the solution
-t_end = 10000.
-t_start = 0.
-t_step = 1.0
-t_interval = np.arange(t_start, t_end+t_step, t_step)
 
-#random intital conditions
-y0 = []
-for i in range(size):
+x_new = []
+
+for trial in range(1):
+
+	L = []
+	Y = []
+	# Initial values
+	for i in range(size):
+		L.append(np.random.random())	# initial values
+		Y.append(0.0)     		# initial memories
+
+	# Bundle parameters for ODE solver
+	params = [a, c, r]
+
+	# Bundle initial conditions for ODE solver
+	y0 = L + Y
+
+	# Make time array for solution
+	tStop = 2000
+	tInc = 0.05	
+	t = np.arange(0., tStop, tInc)
+
+	# Call the ODE solver
+	#psoln = odeint(f, y0, t, args=(params,))
+
+	ode = scipy.integrate.ode(f)
+	ode.set_integrator('dopri5')
+	ode.set_initial_value(y0, 0.0).set_f_params(params)
+
+	#running the solver
+	ts = []
+	psoln = []
+	while ode.successful() and ode.t < tStop:
+		ode.integrate(ode.t + tInc)
+		ts.append(ode.t)
+		psoln.append(ode.y)
+
+	t = np.vstack(ts)
+	psoln = np.array(psoln)		
 	
-	y0.append(np.random.random()*1)
-	y0.append(0.0)
+	traj_list = []
+	for i in range(size):
+		traj_list.append(psoln[:,i])
+		plt.plot(psoln[:,i][500:])	#ignore the first 500 steps
 
-#setting up the integrator
-ode = scipy.integrate.ode(system)
-ode.set_integrator('dopri5')
-ode.set_initial_value(y0, t_start).set_f_params(a, c, r)
+	for step in range(20, len(psoln[:,i])):
+		
+		traj_list.sort(key=lambda x: x[step], reverse=True)
+		if step%20 ==0:		
 
-#running the solver
-ts = []
-sol = []
-while ode.successful() and ode.t < t_end:
+			for traj in traj_list[:20]:
+			
+				#relative_change = traj[step]
+				relative_change = (traj[step-20] - traj[step])/traj[step]
+				if relative_change > 0.00:
+		    			x_new.append(relative_change)
 
-	ode.integrate(ode.t + t_step)
-	ts.append(ode.t)
-	sol.append(ode.y)
+#with open("./relative_change_data_{0}.json".format(r), "wb") as fp:   #Pickling
+#	json.dump(x_new[:], fp)
 
-t = np.vstack(ts)
+plt.show()
 
-#processing the results, binning accoring to stepsize and ploting
-trajectories = []
-y_trajectories = []
+data = pd.Series(x_new)
+# Plot for comparison
+ax = data.plot(kind='hist', normed=True, alpha=0.3, label='Simulation', color='red', loglog=True, bins=np.logspace(np.log10(min(x_new)),np.log10(max(x_new)), 50))
 
-for i in range(len(y0)/2):
-	
-	traj1 = [s[i*2] for s in sol]
-	y_traj1 = [s[i*2+1] for s in sol]
-	#plot the trajectories for checking
-	trajectories.append(traj1)
-	plt.plot(traj1, lw=2)
-	y_trajectories.append(y_traj1)
-	#plt.plot(y_traj1, lw=2)
+with open("../Sunes_twitter/twittertags_relative_top10_full_2016_daily_allyear.txt", "rb") as fp:   # Unpickling
+    b = pickle.load(fp)
+x_new2=[]
+for relhype in b:
+	if relhype > 0.00:
+            x_new2.append(abs(relhype))
 
-plt.xlabel('$t$', fontsize=20)
-plt.ylabel('$L_i(t)$', fontsize=20)
+data2 = pd.Series(x_new2)
+ax = data2.plot(kind='hist', normed=True, alpha=0.3, label='Data', color='black', loglog=True, bins=np.logspace(np.log10(min(x_new2)),np.log10(max(x_new2)), 50))
+plt.legend()
 plt.show()
